8d6a2cf9d9fe6dc03caf526223797ad6763dd71b
iReek <i_r_e_k@mail.ru>
2022-03-01 23:41:50 +0300

    HW2MyFinal

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var changeableView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changeableView.layer.cornerRadius = 10
+        
+        redValue.text = String(format: "%.2f", redSlider.value)
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+        blueValue.text = String(format: "%.2f", blueSlider.value)
     }
 
+    @IBAction func redSliderAction() {
+        redValue.text = String(format: "%.2f", redSlider.value)
+        
+        changeableView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    
+    @IBAction func greenSliderAction() {
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+        
+        changeableView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
 
+        
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValue.text = String(format: "%.2f", greenSlider.value)
+        
+        changeableView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
 }
 
