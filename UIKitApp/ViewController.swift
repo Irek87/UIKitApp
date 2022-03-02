02d7c8e84044dc43bf17d3ee32e3ee5e5b0ee6c8
iReek <i_r_e_k@mail.ru>
2022-03-02 19:34:10 +0300

    HW2MyFinalFianl

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func blueSliderAction() {
-        blueValue.text = String(format: "%.2f", greenSlider.value)
+        blueValue.text = String(format: "%.2f", blueSlider.value)
         
         changeableView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
